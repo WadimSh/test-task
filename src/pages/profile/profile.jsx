c1b2a6b382a3160990e0eef38b18d3f2ec3da42b
WadimSh <wadim11@yandex.ru>
2024-09-08 20:10:59 +0300

    fix

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,5 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
+import style from "./p.module.css";
+
+function PreviewBeforeUpload() {
+  const [file, setFile] = useState(null);
+  const [previewUrl, setPreviewUrl] = useState('');
+  const [fileName, setFileName] = useState('');
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    setFile(file);
+    setPreviewUrl(URL.createObjectURL(file));
+    setFileName(file.name);
+  };
+
+  useEffect(() => {
+    const fileInput = document.getElementById('file-1');
+    fileInput.addEventListener('change', handleFileChange);
+    return () => {
+      fileInput.removeEventListener('change', handleFileChange);
+    };
+  }, []);
+
+  return (
+    <div className={style.form}>
+      <div className={style.grid}>
+        <div className={style.formelement}>
+          <input type="file" id="file-1" accept="image/*" />
+          <label htmlFor="file-1" id="file-1-preview">
+            <img src={previewUrl || 'https://bit.ly/3ubuq5o'} alt="" />
+            <div>
+              <span>+</span>
+            </div>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 
 const Profile = () => {
   let { id } = useParams();
@@ -8,9 +49,12 @@ const Profile = () => {
   const user = users.find(u => u.id === +id);
 
   return (
-    <div className="text text_type_main text_color_primary">
-      <span>{user.name}: {user.email} - {user.password}</span>
-    </div>
+    <>
+      <PreviewBeforeUpload />
+      <div className="text text_type_main text_color_primary">
+        <span>{user.name}: {user.email} - {user.password}</span>
+      </div>
+    </>
   );
 };
 
